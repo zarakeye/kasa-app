88550e49e566f041ae145b0d754409da9b6bbc66
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-12-07 07:09:40 +0100

    commit before clone

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-// /** @type {import('vite').UserConfig} */
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   css: {
