4019559be6212a8ff7f6781125b72a5a49e0c87f
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-12-08 15:20:06 +0100

    All animations perfect

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -12,8 +12,6 @@ const Dropdown: React.FC<DropdownProps> = ({title, content}): JSX.Element => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const arrow = useRef<HTMLImageElement>(null);
   const contentWrapperRef = useRef<HTMLDivElement>(null);
-  const contentRef = useRef<HTMLDivElement>(null);
-
 
   useEffect(() => {
     if (isOpen) {
@@ -23,41 +21,20 @@ const Dropdown: React.FC<DropdownProps> = ({title, content}): JSX.Element => {
     }
   }, [isOpen])
 
-  // const renderContent = () => {
-  //   return Array.isArray(content) ? (
-  //     <ul className={`${style.dropdown_content_list } ${isOpen ? style.open : ''}`}>
-  //       {content.map((item, index) => (
-  //         <li key={index} className={style.dropdown_content_item}>
-  //           {item}
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   ) : (
-  //     <p className={`${style.dropdown_content_text} ${isOpen ? style.open : ''}`}>{content}</p>
-  //   )
-  // }
-
   return (
     <aside className={style.dropdown} >
       <header className={style.dropdown_header} onClick={() => setIsOpen(!isOpen) }>
         <h3 className={style.dropdown_title}>{title}</h3>
         <div className={style.dropdown_arrow_container}>
-          <img ref={arrow} className={style.dropdown_arrow} src={chevron} alt="flèche" />
+          <img ref={arrow} className={`${style.dropdown_arrow } ${isOpen ? style.open : ''}`} src={chevron} alt="flèche" />
         </div>
       </header>
         <div
           ref={contentWrapperRef}
           className={`${style.dropdown_content_wrapper} ${isOpen ? style.open : ''}`}
         >
-        {/* {isOpen && ( */}
-          <div
-            ref={contentRef}
-            className={`${style.dropdown_content_container} ${isOpen ? style.open : ''}`} id={style.dropdown_content}
-          >
-            <DropdownContent content={content} isOpen={isOpen} />
-          </div>
-        {/* )} */}
-      </div>
+          <DropdownContent content={content} isOpen={isOpen} />
+        </div>
     </aside>
   )
 }
