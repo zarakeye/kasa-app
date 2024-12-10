7824aebfa19c9a2e614df9ac75061266813449e5
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-12-10 19:53:24 +0100

    Some JSDoc added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,24 @@ import Error404 from "./pages/Error404"
 import Footer from "./components/Footer"
 import style from './App.module.scss'
 
+
+/**
+ * The root component of the app.
+ *
+ * It renders the following components:
+ * - `Container` containing the `Header` and the routes
+ * - `Footer`
+ *
+ * The routes are defined as follows:
+ * - `/` shows the `Home` component
+ * - `/about` shows the `About` component
+ * - `/flat/:flatId` shows the `Flat` component with the given `flatId`
+ * - `*` shows the `Error404` component with a default error message
+ *
+ * The component uses the `DataContextProvider` context to share the data between the components.
+ *
+ * The component uses the `Router` component to handle the client-side routing.
+ */
 const App: React.FC = () => {
   return (
     <>
@@ -22,10 +40,7 @@ const App: React.FC = () => {
                 <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/about" element={<About />} />
-                  <Route
-                    path="/flat/:flatId"
-                    element={<Flat />}
-                    errorElement={<Error404 message="Oups ! La référence d'appartement que vous demandez n'existe pas" />} />
+                  <Route path="/flat/:flatId" element={<Flat />} />
                   <Route path="*" element={<Error404 message="Oups ! La page que vous demandez n'existe pas" />} />
                 </Routes>
               </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './main.scss'
 import App from './App'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
